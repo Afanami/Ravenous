7335cb8025c583085df1d131f44a6bdc699b8e8f
Afanami <afan6333@gmail.com>
2018-07-11 16:25:47 +1200

    updated name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar';
-import Yelp from './util/Yelp';
+import React, { Component } from "react";
+import "./App.css";
+import BusinessList from "./components/BusinessList/BusinessList";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Yelp from "./util/Yelp";
 
 class App extends Component {
   constructor(props) {
@@ -17,16 +17,16 @@ class App extends Component {
     Yelp.search(term, location, sortBy).then(businesses => {
       this.setState({
         businesses: businesses
-      })
+      });
     });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={this.state.businesses}/>
+        <h1>Yelperia</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
